02d47fbcf30fe0f9ab41f9ab88f3f7207236f7c1
mzaini30 <muhzaini30@gmail.com>
2021-12-25 17:27:50 +0800

    Upload dulu

--- static/data/tools.js
+++ static/data/tools.js
@@ -2,6 +2,7 @@ export default [
 	{
 		nama: 'Olah Tier',
 		slug: 'tier',
-		kategori: 'SEO'
+		kategori: 'SEO',
+		gambar: 'https://i.postimg.cc/28MNRbHz/d26c635d984c44755446121f2a95e904.jpg'
 	}
 ]

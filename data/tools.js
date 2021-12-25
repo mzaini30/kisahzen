ac64521506efa1d107a518b42c0c21f4d7aae80b
mzaini30 <mzaini30@users.noreply.github.com>
2021-12-25 09:28:23 +0000

    deploy: 02d47fbcf30fe0f9ab41f9ab88f3f7207236f7c1

--- data/tools.js
+++ data/tools.js
@@ -1,1 +1,1 @@
-export default[{nama:"Olah Tier",slug:"tier",kategori:"SEO"}];
+export default[{nama:"Olah Tier",slug:"tier",kategori:"SEO",gambar:"https://i.postimg.cc/28MNRbHz/d26c635d984c44755446121f2a95e904.jpg"}];

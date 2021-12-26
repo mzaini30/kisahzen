4d93765234dc0504ee88d3870db837d4b34d6744
mzaini30 <muhzaini30@gmail.com>
2021-12-26 14:16:46 +0800

    QRCode Generator

--- static/data/tools.js
+++ static/data/tools.js
@@ -4,5 +4,11 @@ export default [
 		slug: 'tier',
 		kategori: 'SEO',
 		gambar: 'https://i.postimg.cc/28MNRbHz/d26c635d984c44755446121f2a95e904.jpg'
-	}
+	},
+	{
+		nama: 'QRCode Generator',
+		slug: 'qrcode',
+		kategori: 'Produktivitas',
+		gambar: 'https://i.postimg.cc/XqyGLnQc/1f705f24e1949ac2cf24c252e469af8d.jpg'
+	},
 ]

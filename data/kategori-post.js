041d9132e64645189ab12120469ee01f32721911
mzaini30 <mzaini30@users.noreply.github.com>
2021-12-30 23:48:07 +0000

    deploy: 244e60da2eaaeffd78bdc56e8d277d39dc04cfc9

--- data/kategori-post.js
+++ data/kategori-post.js
@@ -1,1 +1,1 @@
-export default[{kategori:"Dev",subkategori:["Frontend","Text Editor"]},{kategori:"Renungan",subkategori:["Religi"]},{kategori:"Produktif",subkategori:["Menulis","Masak-masak"]}];
+export default[{kategori:"Dev",subkategori:["Frontend","Text Editor"]},{kategori:"Renungan",subkategori:["Religi"]},{kategori:"Produktif",subkategori:["Menulis","Masak-masak","Membaca"]}];

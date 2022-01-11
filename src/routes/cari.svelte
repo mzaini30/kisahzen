90e5cf6f383df23fdcd475fdcd9239d978943de9
mzaini30 <muhzaini30@gmail.com>
2022-01-11 18:35:46 +0800

    Simpan dulu. Cari belum selesai

--- src/routes/cari.svelte
+++ src/routes/cari.svelte
@@ -1,8 +1,29 @@
 <script>
   import { page } from "$app/stores";
   import Meta from "$lib/meta.svelte";
+  import post from '/src/post.js'
+  import {browser} from '$app/env'
+
+
+  let hasil_pencarian = []
 
   let cari = $page.url.searchParams.get("q");
+  async function mulai_cari(){
+    for (let x of post){
+      if (browser) {
+
+        let jelajah = await fetch(`/post/${x.slug}`)
+        jelajah = await jelajah.text()
+        jelajah = jelajah.toLowerCase()
+
+
+        if (jelajah.includes(cari.toLowerCase())) {
+          hasil_pencarian = [...hasil_pencarian, x]
+        }
+      }
+    }
+  }
+  mulai_cari()
 </script>
 
 <svelte:head>
@@ -11,6 +32,7 @@
 </svelte:head>
 
 <h1 class="judul capitalize">Mencari {cari}</h1>
+{JSON.stringify(hasil_pencarian)}
 
 <style lang="windi">
   h1 {
